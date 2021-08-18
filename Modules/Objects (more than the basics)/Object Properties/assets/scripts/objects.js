12e22a4a47a95300e5702832510d8a2b1d0be6b7
Elijah Pekson <elijah.pekson@twala.io>
2021-08-18 22:22:31 +0800

    Objects: adding, modifying, and deleting properties

--- Modules/Objects (more than the basics)/Object Properties/assets/scripts/objects.js
+++ Modules/Objects (more than the basics)/Object Properties/assets/scripts/objects.js
@@ -7,4 +7,16 @@ const person = {
     }
 };
 
-person.greet();
+// adding, modifying, and deleting properties
+console.log(person.isAdmin) // undefined
+person.isAdmin = true;
+console.log(person.isAdmin) // true
+
+person.age = 30;
+console.log(person.age); // 30
+
+delete person.age; // also equivalent to person.age = undefined, but we must never assign undefined to a value
+console.log(person.age); // undefined
+
+person.age = null; // this does NOT delete the age, you just set the property's value to null
+console.log(person.age); // null, and the age property is in the person object
