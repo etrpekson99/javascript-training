a8256ad2eaa72911817b6eea05dd314f9388c4e8
Elijah Pekson <elijah.pekson@twala.io>
2021-08-18 22:30:46 +0800

    Objects: special key names and the square bracket notation

--- Modules/Objects (more than the basics)/Object Properties/assets/scripts/objects.js
+++ Modules/Objects (more than the basics)/Object Properties/assets/scripts/objects.js
@@ -1,3 +1,5 @@
+const movieList = document.getElementById('movie-list');
+
 const person = {
     name:  'Elijah',
     age: 23,
@@ -20,3 +22,22 @@ console.log(person.age); // undefined
 
 person.age = null; // this does NOT delete the age, you just set the property's value to null
 console.log(person.age); // null, and the age property is in the person object
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------
+// special key names and square bracket property access
+// JS coerces keys into strings
+// therefore we can use strings as keys
+const anotherPerson = {
+    'first-name': 'Elijah',
+    age: 23,
+    hobbies: ['Coding', 'Cooking'],
+    greet: function() {
+        console.log('hi there');
+    }
+}
+
+// this is called the square bracket notation, this is available on any object
+// square bracket notations will accept constants with values that correspond to a key
+console.log(anotherPerson['first-name']); // Elijah
+movieList.style['background-color'] = 'red';
+movieList.style.display = 'block';
