4b5c9b856843139b9b8e2b4242298a78e8f90b90
Elijah Pekson <elijah.pekson@twala.io>
2021-08-21 20:59:29 +0800

    OOP: using and "connecting" multiple classes

--- Modules/Object-oriented programming/Getting started with OOP/assets/scripts/app.js
+++ Modules/Object-oriented programming/Getting started with OOP/assets/scripts/app.js
@@ -13,32 +13,48 @@ class Product {
     }
 }
 
-const productList = {
-    products: [
+class ProductItem {
+    constructor(product) {
+        this.product = product;
+    }
+
+    render() {
+        const prodEl = document.createElement('li');
+        prodEl.className = 'product-item';
+        prodEl.innerHTML = `
+            <div>
+                <img src="${this.product.imageUrl}" />
+                <div class="product-item__content" alt="${this.product.title}">
+                    <h2>${this.product.title}</h2>
+                    <h3>${this.product.price}</h3>
+                    <p>${this.product.description}</p>
+                </div>
+            </div>
+        `;
+        return prodEl;
+    }
+}
+
+class ProductList {
+    products = [
         new Product('Pillow', 'https://cf.shopee.ph/file/3202d1df80b4929c0fc595bd473eb842', 19.99, 'a soft pillow'),
         new Product('Carpet', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Wollteppich_1.jpg/1200px-Wollteppich_1.jpg', 89.99, 'a nice carpet'),
-    ],
+    ];
+
+    constructor() {}
+
     render() {
         const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for (const prod of this.products) {
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
-                <div>
-                    <img src="${prod.imageUrl}" />
-                    <div class="product-item__content" alt="${prod.title}">
-                        <h2>${prod.title}</h2>
-                        <h3>${prod.price}</h3>
-                        <p>${prod.description}</p>
-                    </div>
-                </div>
-            `;
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
     }
 }
 
+const productList = new ProductList();
 productList.render();
