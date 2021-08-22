6a0e8a2662ffd3bca79823ee83597b50cedf36f4
Elijah Pekson <elijah.pekson@twala.io>
2021-08-22 22:59:25 +0800

    Practice (OOP): starting with the switch project logic

--- Modules/Practice: OOP and Classes/assets/scripts/app.js
+++ Modules/Practice: OOP and Classes/assets/scripts/app.js
@@ -6,13 +6,40 @@ class Tooltip {
 }
 
 class ProjectItem {
+    constructor(id) {
+        this.id = id;
+        this.connectMoreInfoButton();
+        this.connectSwitchButton();
+    }
 
+    connectMoreInfoButton() {
+
+    }
+
+    connectSwitchButton() { // switch from finished to active and vice-versa
+        const projectItemElement = document.getElementById(this.id);
+        const switchButton = projectItemElement.querySelector('button:last-of-type');
+        switchButton.addEventListener('click', )
+    }
 }
 
 class ProjectList {
+    projects = [];
+
     constructor(type) {
-        const projItems = document.querySelectorAll(`#${type}-projects li`); // all list items
-        console.log(projItems);
+        const projectItems = document.querySelectorAll(`#${type}-projects li`); // all list items
+        for (const projectItem of projectItems) {
+            this.projects.push(new ProjectItem(projectItem.id)); // id of the DOM node
+        }
+    }
+
+    addProject() { // add the project to it's new list
+
+    }
+
+    switchProject(projectId) { // remove the project from it's existing list
+        const projectIndex = this.projects.findIndex(p => p.id === projectId);
+        this.projects.splice(projectIndex, 1);
     }
 }
 
