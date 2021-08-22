a9f470b0d4d60479744497516a6cfcd8556504df
Elijah Pekson <elijah.pekson@twala.io>
2021-08-22 20:14:56 +0800

    Deep dive: working with prototypes

--- Modules/Deep dive: Constructor functions and prototypes/Constructor functions/app.js
+++ Modules/Deep dive: Constructor functions and prototypes/Constructor functions/app.js
@@ -1,7 +1,17 @@
-class Person { // this class here is just syntactical sugar
+class AgedPerson { // this class here is just syntactical sugar
+    printAge() {
+        console.log(this.age);
+    }
+}
+
+// what this does is that it creates an object based on
+// the AgedPerson class, and then sets it as a prototype
+// to the object based on Person class
+class Person extends AgedPerson {
     name = 'Elijah';
 
     constructor() {
+        super();
         this.age = 23;
     }
 
@@ -26,8 +36,38 @@ function PersonFunc() {
     // return this;
 }
 
+// prototype property of the function object can be used to assign an object
+// which then will be assigned as a prototype to any objects we build 
+// based on the constructor function.
+// we replace the old default object which is assigned as an object
+// with a new object
+PersonFunc.prototype = {
+    printAge() {
+        console.log(this.age); // "this" always refers to the object on which we called the method
+    }
+}
+
+// an alternative and better approach would be to manually add
+// printAge like this:
+// PersonFunc.prototype.printAge = function() {
+//      console.log(this.age);
+// }
+
 const personFunc = new PersonFunc();
 personFunc.greet();
 
 const person = new Person();
 person.greet();
+
+console.log(personFunc.toString()); // this will work because of prototypes
+// our constructor function also points to an "invisible" function
+// console.log(person.toStr()); // this will NOT work
+
+// this is the default object every custom constructor function
+// assigns to an object based on it
+personFunc.printAge();
+console.log(personFunc.toString());
+console.log(person.__proto__);
+
+// prototype in Person.prototype is not a fallback for the Person class / constructor function
+console.log(person.__proto__ === Person.prototype); // true, 
