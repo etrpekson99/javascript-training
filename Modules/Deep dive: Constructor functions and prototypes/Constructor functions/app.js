d9afe609dbf542e9f16eeb1b7db5ebd881af234f
Elijah Pekson <elijah.pekson@twala.io>
2021-08-22 18:39:49 +0800

    Deep dive: constructor functions vs classes and understanding "new"

--- Modules/Deep dive: Constructor functions and prototypes/Constructor functions/app.js
+++ Modules/Deep dive: Constructor functions and prototypes/Constructor functions/app.js
@@ -1,4 +1,4 @@
-class Person {
+class Person { // this class here is just syntactical sugar
     name = 'Elijah';
 
     constructor() {
@@ -10,14 +10,24 @@ class Person {
     }
 }
 
-// the class above would typically be written like this
+// the class above would typically be written like this, a constructor function
+// we can't expect that this function returns an object
+// it will only return an object because of the "new" keyword
+// calling it like this const personFunc = PersonFunc() won't work
 function PersonFunc() {
+    // behind the scenes, the "new" keyword sets "this" to an empty object
+    // this = {};
     this.age = 23;
     this.name = 'Elijah';
     this.greet = function() {
         console.log(`Hi I am ${this.name} and I am ${this.age} years old`);
     }
+    // also behind the scenes is the function returning "this" when "new" is used
+    // return this;
 }
+
+const personFunc = new PersonFunc();
+personFunc.greet();
 
 const person = new Person();
 person.greet();
