17c3adfcdc617ea6cdeaaf16f958ee012e580f58
Elijah Pekson <elijah.pekson@twala.io>
2021-08-22 22:48:09 +0800

    Practice (OOP): creating project lists and parsing element data

--- Modules/Practice: OOP and Classes/assets/scripts/app.js
+++ Modules/Practice: OOP and Classes/assets/scripts/app.js
@@ -1,3 +1,6 @@
+const ACTIVE = 'active';
+const FINISHED = 'finished';
+
 class Tooltip {
 
 }
@@ -7,9 +10,17 @@ class ProjectItem {
 }
 
 class ProjectList {
-
+    constructor(type) {
+        const projItems = document.querySelectorAll(`#${type}-projects li`); // all list items
+        console.log(projItems);
+    }
 }
 
 class App {
-    
+    static init() {
+        const activeProjectsList = new ProjectList(ACTIVE);
+        const finishedProjectsList = new ProjectList(FINISHED);
+    }
 }
+
+App.init();
