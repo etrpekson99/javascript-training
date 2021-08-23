f2d68749f9f5a2931a1bb96adb01e2fcdffed767
Elijah Pekson <elijah.pekson@twala.io>
2021-08-23 21:56:04 +0800

    More on the DOM & Browser APIs: handling scrolling

--- Modules/More on the DOM and Browser APIs/assets/scripts/app.js
+++ Modules/More on the DOM and Browser APIs/assets/scripts/app.js
@@ -15,6 +15,7 @@ class DOMHelper {
         const element = document.getElementById(elementId);
         const destinationElement = document.querySelector(newDestinationSelector);
         destinationElement.append(element);
+        element.scrollIntoView({ behavior: 'smooth' }); // with a tiny animation
     }
 }
 
