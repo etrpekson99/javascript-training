c40f97c66bef1ba20cf97ce82fca7684ab37e986
Elijah Pekson <elijah.pekson@twala.io>
2021-08-24 23:20:11 +0800

    Working with Events: removing event listeners

--- Modules/Working with Events/Events Basics/assets/scripts/events.js
+++ Modules/Working with Events/Events Basics/assets/scripts/events.js
@@ -19,7 +19,17 @@ const anotherButtonClickHandler = () => {
 // button.onclick = buttonClickHandler;
 // button.onclick = anotherButtonClickHandler; // this will overried the previous listener
 
+// this is still the most elegant way of doing this
 // allows us to add multiple event listeners to the same element
-button.addEventListener(); 
+button.addEventListener('click', buttonClickHandler); 
 
-// button.removeEventListener();
+setTimeout(() => {
+    button.removeEventListener('click', buttonClickHandler);
+    // a couple of pitfalls of removeEventListener:
+    // - will not work if we use an anonymous function since we will
+    //   use two different functions from add and remove event listener
+    // - we have to make sure we store the event handler functions
+    //   in constants
+    // - we will again have to store the bound function in a new constant:
+    //   const boundFn = buttonClickHandler.bind(this);
+}, 2000)
