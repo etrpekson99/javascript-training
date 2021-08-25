f9deeb775aaa15400e032d7055588eb7e026d5e5
Elijah Pekson <elijah.pekson@twala.io>
2021-08-26 01:05:29 +0800

    More on Numbers & Strings: how numbers work in JS

---
+++ Modules/More on Numbers and Strings/Numbers/app.js
@@ -0,0 +1,9 @@
+// the biggest possible integer we can reliably represent with a normal number type in JS
+// calculations with this number will not work
+console.log(Number.MAX_SAFE_INTEGER); 
+
+// smallest possible integer we can reliably represent with a normal number type in JS
+console.log(Number.MIN_SAFE_INTEGER); 
+
+// largest value we can represent in JS
+console.log(Number.MAX_VALUE);
