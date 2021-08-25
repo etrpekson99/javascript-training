799eb75432abd6f78e761ae5ecb5b8f41862fba7
Elijah Pekson <elijah.pekson@twala.io>
2021-08-26 00:33:47 +0800

    More on Functions: introducing recursion

--- Modules/Advanced Function concepts/app.js
+++ Modules/Advanced Function concepts/app.js
@@ -93,3 +93,28 @@ greetUser(); // this will output "Hi Hannah"
 // the basically track variable usage and if a variable isn't getting
 // used by anything; not by any functions and nowhere else,
 // the JS engine will get rid of it
+
+// --------------------------------------------------------------------------------------------------------
+
+// introducing recursion
+
+// function powerOf(x, n) {
+//     let result = 1;
+//     for (let i = 0; i < n; i++) {
+//         result *= x;
+//     }
+
+//     return result;
+// }
+
+// recursion implementation
+function powerOf(x, n) {
+    // if (n === 1) {
+    //     return x;
+    // }
+
+    // return x * powerOf(x, n - 1);
+    return n === 1 ? x : x * powerOf(x, n - 1);
+}
+
+console.log(powerOf(2, 3)); // 2 * 2 * 2
