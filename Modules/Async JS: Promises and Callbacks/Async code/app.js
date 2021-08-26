f2c352fb256e4b19a119299f90ca75ab4662fb23
Elijah Pekson <elijah.pekson@twala.io>
2021-08-27 00:29:41 +0800

    Async JS: sync + async code - the execution order

--- Modules/Async JS: Promises and Callbacks/Async code/app.js
+++ Modules/Async JS: Promises and Callbacks/Async code/app.js
@@ -3,7 +3,12 @@ const output = document.querySelector('p');
 
 // this function is only executed when the for-loop below is finished
 function trackUserHandler() {
-  console.log('Clicked!');
+  navigator.geolocation.getCurrentPosition(posData => { // these functions are handed off to the browser
+    console.log(posData);
+  }, error => {
+    console.log(error);
+  });
+  console.log('getting position...'); // this goes into the call stack first
 }
 
 // when we call this line, we're telling the browser to setup
@@ -14,13 +19,15 @@ button.addEventListener('click', trackUserHandler); // trackUserHandler is the f
 
 // blocking code and the "event loop"
 
-let result = 0;
+// let result = 0;
 // all clicks are processed only when the loop is done
 // JS is blocked until this code is done.
 // this occupies the call stack, therefore the function
 // attached to the event above does not fire immediately
-for(let i = 0; i < 100000000; i++) {
-  result += i;
-}
+// for(let i = 0; i < 100000000; i++) {
+//   result += i;
+// }
 
-console.log(result);
+// console.log(result);
+
+// ------------------------------------------------------------------------------
