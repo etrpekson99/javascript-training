2ed02ef79378ca0dd8db8c5c2557a6b3b310be1f
Elijah Pekson <elijah.pekson@twala.io>
2021-08-28 23:56:06 +0800

    HTTP Requests: JSON data, parsing data, and outputting request results

--- Modules/HTTP Requests/Getting Started/assets/scripts/app.js
+++ Modules/HTTP Requests/Getting Started/assets/scripts/app.js
@@ -1,5 +1,23 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
 const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts'); // accepts the method and the URL
+
+xhr.responseType = 'json'; // parses the response for us
+
+xhr.onload = () => {
+    // const listOfPosts = JSON.parse(xhr.response); // we get JSON data, therefore we have to parse it
+    const listOfPosts = xhr.response;
+    
+    for (post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector('h2').textContent = post.title;
+        postEl.querySelector('p').textContent = post.body;
+
+        listElement.append(postEl);
+    }
+}
 
 xhr.send(); // this sends the request
