0f2a724a07fb65465ebd2fe04ed8e3dede32c5e6
Elijah Pekson <elijah.pekson@twala.io>
2021-08-29 21:57:27 +0800

    JS Modules: first step towards JS modules

--- Modules/Modular JS/assets/scripts/app.js
+++ Modules/Modular JS/assets/scripts/app.js
@@ -10,12 +10,6 @@ class App {
     finishedProjectsList.setSwitchHandlerFunction(
       activeProjectsList.addProject.bind(activeProjectsList)
     );
-
-    // const timerId = setTimeout(this.startAnalytics, 3000);
-
-    // document.getElementById('stop-analytics-btn').addEventListener('click', () => {
-    //   clearTimeout(timerId);
-    // });
   }
 
   static startAnalytics() {
