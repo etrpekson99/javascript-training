56e758157bba3151e2ec6f208ab509c4839eee52
Elijah Pekson <elijah.pekson@twala.io>
2021-08-29 22:09:14 +0800

    JS Modules: switching all files to use modules

--- Modules/Modular JS/assets/scripts/App/ProjectList.js
+++ Modules/Modular JS/assets/scripts/App/ProjectList.js
@@ -1,4 +1,7 @@
-class ProjectList {
+import { ProjectItem } from './ProjectItem.js';
+import { DOMHelper } from '../Utility/DOMHelper.js';
+
+export class ProjectList {
     projects = [];
   
     constructor(type) {
@@ -60,8 +63,6 @@ class ProjectList {
     }
   
     switchProject(projectId) {
-      // const projectIndex = this.projects.findIndex(p => p.id === projectId);
-      // this.projects.splice(projectIndex, 1);
       this.switchHandler(this.projects.find(p => p.id === projectId));
       this.projects = this.projects.filter(p => p.id !== projectId);
     }
