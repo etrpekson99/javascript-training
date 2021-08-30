1a3108e13bf368bbe04d6ddfce6c84f0fbc4c5d2
Elijah Pekson <etrpekson1998@gmail.com>
2021-08-30 20:46:47 +0800

    JS Tooling & Workflows: development mode and fixing 'lazy loaded' JS files

--- Modules/JS Tooling and Workflows/webpack.config.js
+++ Modules/JS Tooling and Workflows/webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/app.js',
     output: {
         filename: 'app.js',
         // constructs a new absolute path starting at our current file
         path: path.resolve(__dirname, 'assets', 'scripts'),
+        // tell webpack that our files are stored in a different path
+        publicPath: 'assets/scripts/'
     }
 };
