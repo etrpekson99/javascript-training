73b598e57dcd2ef13f301910cf051ab9b2b92aa7
Elijah Pekson <elijah.pekson@twala.io>
2021-08-31 01:47:40 +0800

    Browser Storage: getting started with IndexedDB

---
+++ Modules/Utilizing Browser Storage/IndexDB/app.js
@@ -0,0 +1,37 @@
+const storeBtn = document.getElementById('store-btn');
+const retrieveBtn = document.getElementById('retrieve-btn');
+
+// we pass the name of the db and the version
+const dbRequest = indexedDB.open('StorageDummy', 1);
+
+// we can add an event listener, but this is for better browser support
+dbRequest.onupgradeneeded = function(event) {
+    const db = event.target.result; // holds access to the db created
+
+    const store = db.createObjectStore('products', {
+        keyPath: 'id', // unique identifier
+    });
+
+    store.transaction.oncomplete = function() { // triggers when the store creation finishes
+        const productsStore = db.transaction('products', 'readwrite').objectStore('products');
+
+        productsStore.add({
+            id: 'p1', // this is required
+            title: 'pillow',
+            price: 5.99,
+            tags: ['expensive', 'comfortable'],
+        });
+    }
+};
+
+dbRequest.onerror = function() {
+    console.log('error');
+};
+
+storeBtn.addEventListener('click', () => {
+    
+});
+
+retrieveBtn.addEventListener('click', () => {
+     
+});
