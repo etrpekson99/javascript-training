633afb778775edb1dc395cea9cce6ccb8efb13e0
Elijah Pekson <elijah.pekson@twala.io>
2021-08-30 20:19:57 +0800

    JS Tooling & Workflows: linting with ESLint

--- Modules/JS Tooling and Workflows/assets/scripts/Utility/Analytics.js
+++ Modules/JS Tooling and Workflows/assets/scripts/Utility/Analytics.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-magic-numbers */
+/* eslint-disable function-call-argument-newline */
 
 const intervalId = setInterval(() => {
-  console.log('Sending analytics data...')
+  // eslint-disable-next-line no-console
+  console.log('Sending analytics data...');
 }, 2000);
 
 document.getElementById('stop-analytics-btn').addEventListener('click', () => {
