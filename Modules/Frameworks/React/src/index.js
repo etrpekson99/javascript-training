38046eac150267e7cf693b13fe821829f1455f86
Elijah Pekson <elijah.pekson@twala.io>
2021-09-01 01:25:58 +0800

    Frameworks: a React project

---
+++ Modules/Frameworks/React/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+// we have a single page application
+ReactDOM.render(<App />, document.getElementById('root'));
