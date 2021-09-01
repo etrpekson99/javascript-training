464be696a398c1482f403fc694102eb5ac73bd05
Elijah Pekson <elijah.pekson@twala.io>
2021-09-02 00:00:17 +0800

    Meta Programming: understanding iterators

--- Modules/Meta-programming/app.js
+++ Modules/Meta-programming/app.js
@@ -10,7 +10,7 @@ const user = {
     [uid]: 'p1', // the user cannot tamper with this property
     name: 'Elijah',
     age: 23,
-    [Symbol.toStringTag]: 'User', 
+    [Symbol.toStringTag]: 'User', // allows us to "tweak" the output of the toString() method
 }
 
 // using the library
@@ -26,3 +26,33 @@ console.log(Symbol('uid') === Symbol('uid')); // false
 
 // (some) well-known symbols
 console.log(user.toString()); // [object User]
+
+// ----------------------------------------------------------------
+// iterators
+
+const company = {
+    currEmployee: 0,
+    employees: ['Eli', 'Raph', 'EJ'],
+    next() {
+        if (this.currEmployee >= this.employees.length) {
+            return { value: this.currEmployee, done: true };
+        }
+        const returnValue = { value: this.employees[this.currEmployee], done: false };
+        this.currEmployee++;
+        return returnValue;
+    }
+};
+
+// console.log(company.next());
+// console.log(company.next());
+// console.log(company.next());
+// console.log(company.next());
+// console.log(company.next());
+// console.log(company.next()); // we "exhaust" a list of data until it is "done"
+
+let employee = company.next();
+
+while(!employee.done) {
+    console.log(employee.value);
+    employee = company.next();
+}
