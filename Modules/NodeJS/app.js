9264f525db920da4f47a622ea49e64d96a34850e
Elijah Pekson <elijah.pekson@twala.io>
2021-09-02 22:22:01 +0800

    NodeJS: modules and file access

--- Modules/NodeJS/app.js
+++ Modules/NodeJS/app.js
@@ -1,5 +1,26 @@
+const fs = require('fs');
+
 const userName = 'Eli';
 
 console.log(`hi ${userName}`);
 
 // document.querySelector('div'); // this will not work in node
+
+// ----------------------------------------------------------------------------
+
+// modules and file access
+fs.writeFile('user-data.txt', 'username=Elijah', err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('wrote to file');
+    }
+});
+
+fs.readFile('user-data.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(data.toString());
+})
