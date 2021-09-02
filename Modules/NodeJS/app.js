f4132ede0e3407a1a895cf91ba485847f6f5b89e
Elijah Pekson <elijah.pekson@twala.io>
2021-09-03 00:36:59 +0800

    NodeJS: Understanding CORS

--- Modules/NodeJS/app.js
+++ Modules/NodeJS/app.js
@@ -8,6 +8,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    // tell the server that requests from this domain is allowed
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
 app.use(locationRoutes);
 
 
