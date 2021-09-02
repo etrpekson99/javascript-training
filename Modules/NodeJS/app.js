6d7919c461faa9fcd8a3c62426687dba1cc0d67f
Elijah Pekson <elijah.pekson@twala.io>
2021-09-02 22:59:39 +0800

    NodeJS: sending responses (HTML data)

--- Modules/NodeJS/app.js
+++ Modules/NodeJS/app.js
@@ -2,7 +2,9 @@ const http = require('http');
 
 // creates an http server
 const server = http.createServer((request, response) => { // this function triggers for every incoming request
-    response.write('hello there!');
+    response.setHeader('Content-Type', 'text/html'); // this is metadata returned to the browser that says the type of the data returned is text/html
+    // response.setHeader('Content-Type', 'text/plain'); // the browser will render this as plain text, not html
+    response.write('<p>hello there!</p>');
     response.end(); // we are done adding data to the response
 });
 
