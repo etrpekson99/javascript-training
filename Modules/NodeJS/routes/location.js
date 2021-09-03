ca43de76511695340cdd9e7dfd94ad77f0e05489
Elijah Pekson <elijah.pekson@twala.io>
2021-09-03 23:38:05 +0800

    NodeJS: interacting with MongoDB

--- Modules/NodeJS/routes/location.js
+++ Modules/NodeJS/routes/location.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongodb = require('mongodb');
+const { MongoClient } = require("mongodb");
 
 const router = express.Router();
+
+const uri =
+  "mongodb+srv://elijah:password@share-my-place-cluster.p8d8b.mongodb.net/locations?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
 
 const locationStorage = {
     locations: [],
@@ -8,40 +15,49 @@ const locationStorage = {
 
 // only incoming post requests to add-location will make it here
 router.post('/add-location', (req, res, next) => {
-    const id = Math.random();
     // we can also add validation here
 
-    locationStorage.locations.push({
-        id,
-        address: req.body.address,
-        coordinates: {
-            lat: req.body.lat,
-            lng: req.body.lng
-        }
-    });
+    client.connect(function(err, client) {
+        const db = client.db('locations');
 
-    // send a json response
-    res.json({
-        message: 'Stored location',
-        locationId: id,
+        // Insert a single document
+        db.collection('user-locations').insertOne(
+            {
+                address: req.body.address,
+                coords: { lat: req.body.lat, lng: req.body.lng }
+            },
+            function(err, r) {
+                // if (err) {}
+                console.log(r);
+                res.json({ message: 'Stored location!', locationId: r.insertedId });
+            }
+        );
     });
 });
 
 router.get('/location/:locationId', (req, res, next) => {
-    const locationId = +req.params.locationId;
-    const location = locationStorage.locations.find(loc => {
-        return loc.id === locationId;
-    });
+    const locationId = req.params.locationId;
 
-    if (!location) {
-        res.status(404).json({ // return an error response to the FE
-            message: 'not found',
-        });
-    }
+    client.connect(function(err, client) {
+        const db = client.db('locations');
+    
+        // Insert a single document
+        db.collection('user-locations').findOne(
+          {
+            _id: new mongodb.ObjectId(locationId) // ObjectId is a mongodb-specific data type
+          },
+          function(err, doc) {
+            // if (err) {}
+            if (!doc) {
+              return res.status(404).json({ message: 'Not found!' });
+            }
 
-    res.json({
-        address: location.address,
-        coordinates: location.coordinates,
+            res.json({
+                address: doc.address,
+                coordinates: doc.coords,
+            });
+          }
+        );
     });
 });
 
