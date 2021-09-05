030f4ceca9d72e432a235ae3734367ad4a13d757
Elijah Pekson <elijah.pekson@twala.io>
2021-09-05 22:42:32 +0800

    setup code for optimization

---
+++ Modules/Performance Optimizations/src/optimized/shop.js
@@ -0,0 +1,14 @@
+import { initProducts } from './product-management';
+
+const addProduct = (event) => {
+    import('./product-management.js')
+    .then(module => {
+        module.addProduct(event);
+    });
+}
+
+const addProductForm = document.getElementById('new-product');
+
+initProducts();
+
+addProductForm.addEventListener('submit', addProduct);
