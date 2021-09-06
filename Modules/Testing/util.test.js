d08af262d613993e11b70ab0a417397db76e3a9f
Elijah Pekson <elijah.pekson@twala.io>
2021-09-07 01:45:56 +0800

    Testing: writing and running integration tests

--- Modules/Testing/util.test.js
+++ Modules/Testing/util.test.js
@@ -1,6 +1,6 @@
 // jest automatically detects files with .test and runs them
 
-const { generateText } = require('./util'); // this is the native way of importing in jest tests
+const { generateText, checkAndGenerate } = require('./util'); // this is the native way of importing in jest tests
 
 // test() is globally available via jest
 test('should output name and age', () => { // this is the function jest executes to run a test
@@ -19,4 +19,17 @@ test('should output data that was passed', () => { // check for empty values
 
     const text2 = generateText();
     expect(text2).toBe('undefined (undefined years old)');
-})
+});
+
+test('should generate a valid text output', () => {
+    const text = checkAndGenerate('Eli', 23);
+    expect(text).toBe("Eli (23 years old)");
+});
+
+test('should return false', () => {
+    const text = checkAndGenerate(null, 23);
+    expect(text).toBe(false);
+
+    const text1 = checkAndGenerate('Eli', null);
+    expect(text1).toBe(false);
+});
