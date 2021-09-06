100abe532e32d54ebeee5e07d3af9df3ab796a25
Elijah Pekson <elijah.pekson@twala.io>
2021-09-07 01:36:21 +0800

    Testing: writing and running unit tests

---
+++ Modules/Testing/util.test.js
@@ -0,0 +1,22 @@
+// jest automatically detects files with .test and runs them
+
+const { generateText } = require('./util'); // this is the native way of importing in jest tests
+
+// test() is globally available via jest
+test('should output name and age', () => { // this is the function jest executes to run a test
+    const text = generateText('Elijah', 23);
+    expect(text).toBe("Elijah (23 years old)");
+    
+    // expect(text).toBe("Elijah (21 years old)"); // this will show a fail
+
+    const text2 = generateText('Hannah', 23);
+    expect(text2).toBe("Hannah (23 years old)");
+});
+
+test('should output data that was passed', () => { // check for empty values
+    const text = generateText('', null);
+    expect(text).toBe(' (null years old)');
+
+    const text2 = generateText();
+    expect(text2).toBe('undefined (undefined years old)');
+})
