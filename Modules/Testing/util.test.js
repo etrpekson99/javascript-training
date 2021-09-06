73124e636862e8ea8a6c4afc989613148405ee0b
Elijah Pekson <elijah.pekson@twala.io>
2021-09-07 02:06:56 +0800

    Testing: running and writing E2E tests

--- Modules/Testing/util.test.js
+++ Modules/Testing/util.test.js
@@ -1,5 +1,5 @@
 // jest automatically detects files with .test and runs them
-
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util'); // this is the native way of importing in jest tests
 
 // test() is globally available via jest
@@ -33,3 +33,26 @@ test('should return false', () => {
     const text1 = checkAndGenerate('Eli', null);
     expect(text1).toBe(false);
 });
+
+test('should create a new element', async () => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        // slowMo: 80,
+        // args: ['--window-size=1920,1080']
+    });
+
+    const page = await browser.newPage();
+    await page.goto('file:///Users/elijahtristanpekson/Documents/programming/javascript-training/Modules/Testing/index.html');
+
+    await page.click('input#name');
+    await page.type('input#name', 'Hannah');
+
+    await page.click('input#age');
+    await page.type('input#age', '23');
+    // await this.page.waitFor(2000);
+    await page.click('#btnAddUser');
+    // await page.$eval("#btnAddUser", (elem) => elem.click());
+
+    const finalText = await page.$$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe('Hannah (23 years old)');
+}, 10000);
