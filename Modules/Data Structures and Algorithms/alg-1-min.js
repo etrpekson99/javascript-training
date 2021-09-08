70553a058e29461909d68cd8085df6106402016e
Elijah Pekson <elijah.pekson@twala.io>
2021-09-08 23:58:33 +0800

    Data Structures and Algorithms: solving the same problem differently

--- Modules/Data Structures and Algorithms/alg-1-min.js
+++ Modules/Data Structures and Algorithms/alg-1-min.js
@@ -14,8 +14,34 @@ const getMin = (numbers) => {
     return currentMin;
 }
 
+const getMin2 = (numbers) => {
+    if (!numbers.length) {
+        throw new Error('should not be an empty array');
+    }
+
+    for (let i = 0; i < numbers.length; i++) {
+        let outerElement = numbers[i];
+        for(let j = i + 1; j < numbers.length; j++) {
+            let innerElement = numbers[j];
+
+            if (outerElement > innerElement) {
+                // swap
+                numbers[i] = innerElement;
+                numbers[j] = outerElement;
+
+                outerElement = numbers[i];
+                innerElement = numbers[j];
+            }
+        }
+    }
+
+    return numbers[0];
+}
+
 const testNumbers = [3, 7, 2, 8, -15, 1];
+const testNumbers2 = [3, 1, 2];
 
 const min = getMin(testNumbers);
+const min2 = getMin2(testNumbers);
 
-console.log(min); // should be 1
+console.log(min, min2); // should be 1
